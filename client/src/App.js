5a9493c3d30477abbe2bcd3597884f2960fce171
JHL71 <neko0717@naver.com>
2022-01-20 11:02:29 +0900

    Signup progress
    Signup 진행중 동기

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,22 @@ import Profile from './pages/Profile';
 import Dashboard from './pages/Dashboard';
 import { Routes, Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 
+import Signup from './pages/Signup'
+
 function App () {
   return (
     <Router>
-
       <Header />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/sp' element={<Signup />} />
         {/* {isLogin ? <Redirect to='' */}
       </Routes>
       <Footer />
-
     </Router>
+
 
   );
 }
