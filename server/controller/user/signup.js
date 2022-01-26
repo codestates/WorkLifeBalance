5b9e7394b7fe37b657164dcfc2f38a93aa1157de
torangL <yeonlee199900@gmail.com>
2022-01-26 10:13:02 +0900

    Modify: server controller, Add: task/list index query

--- server/controller/user/signup.js
+++ server/controller/user/signup.js
@@ -4,22 +4,26 @@ const { generateAccessToken, sendAccessToken } = require('../tokenFunctions');
 
 module.exports = {
   post: async (req, res) => {
-    const { userId, name, password, email } = req.body;
-    if (!(email && userId && password && email)) {
-      return res.status(400).send({ message: 'bad request' });
+    try {
+      const { userId, name, password, email } = req.body;
+      if (!(email && userId && password && email)) {
+        return res.status(400).send({ message: 'bad request' });
+      }
+      const hashPassword = crypto.createHash('sha512').update(password).digest('hex');
+      const [userInfo, created] = await Users.findOrCreate({
+        where: { email },
+        defaults: { email, userId, password: hashPassword, name },
+        raw: true
+      });
+      if (!created) {
+        return res.status(409).send({ message: 'email exists' });
+      }
+      delete userInfo.dataValues.password;
+      const jwt = generateAccessToken(userInfo.dataValues);
+      sendAccessToken(res, jwt);
+      return res.status(201).send(userInfo.dataValues);
+    } catch (err) {
+      res.status(500).send({ message: 'server error' });
     }
-    const hashPassword = crypto.createHash('sha512').update(password).digest('hex');
-    const [userInfo, created] = await Users.findOrCreate({
-      where: { email },
-      defaults: { email, userId, password: hashPassword, name },
-      raw: true
-    });
-    if (!created) {
-      return res.status(409).send({ message: 'email exists' });
-    }
-    const jwt = generateAccessToken(userInfo.dataValues);
-    delete userInfo.dataValues.password;
-    sendAccessToken(res, jwt);
-    return res.status(201).send(userInfo.dataValues);
   }
 };
--- server/controller/user/updateInfo.js
+++ server/controller/user/updateInfo.js
@@ -5,40 +5,44 @@ module.exports = {
   post: async (req, res) => {
     // Cookie Header 존재 여부, 유효한 JWT 토큰 탐색
     // 토큰의 ID가 가입이 된 ID인지(Users 모델에서 일치하는 ID가 있는지) 검색
-    const userInfo = isAuthorized(req);
-    if (!userInfo) {
-      return res.status(400).send({ message: 'bad request' });
-    } else {
-      const { id } = userInfo;
-      const user = await Users.findOne({
-        attributes: ['id', 'userId', 'email', 'name', 'createdAt', 'updatedAt'],
-        where: { id }
-      });
-
-      if (!user) { return res.status(401).send({ message: 'not authorized' }); } else {
-        // 인증 후 Users 모델에서 해당 userId 관련 정보 수정
-        const [userId, name, email] = [req.body.userId, req.body.name, req.body.email];
-        const updateInfo = {
-          userId: userId,
-          name: name,
-          email: email
-        };
-        await Users.update(updateInfo, {
+    try {
+      const userInfo = isAuthorized(req);
+      if (!userInfo) {
+        return res.status(400).send({ message: 'bad request' });
+      } else {
+        const { id } = userInfo;
+        const user = await Users.findOne({
+          attributes: ['id', 'userId', 'email', 'name', 'createdAt', 'updatedAt'],
           where: { id }
         });
 
-        const data = await Users.findOne({
-          attributes: ['id', 'userId', 'email', 'name', 'createdAt', 'updatedAt'],
-          where: { id: id }
-        });
-        res.clearCookie('jwt');
-        const jwt = generateAccessToken(data.dataValues);
-        sendAccessToken(res, jwt);
-        return res.status(200).send({
-          data: data.dataValues,
-          message: 'user info successfully modified'
-        });
+        if (!user) { return res.status(401).send({ message: 'not authorized' }); } else {
+          // 인증 후 Users 모델에서 해당 userId 관련 정보 수정
+          const [userId, name, email] = [req.body.userId, req.body.name, req.body.email];
+          const updateInfo = {
+            userId: userId,
+            name: name,
+            email: email
+          };
+          await Users.update(updateInfo, {
+            where: { id }
+          });
+
+          const data = await Users.findOne({
+            attributes: ['id', 'userId', 'email', 'name', 'createdAt', 'updatedAt'],
+            where: { id: id }
+          });
+          res.clearCookie('jwt');
+          const jwt = generateAccessToken(data.dataValues);
+          sendAccessToken(res, jwt);
+          return res.status(200).send({
+            data: data.dataValues,
+            message: 'user info successfully modified'
+          });
+        }
       }
+    } catch (err) {
+      res.status(500).send({ message: 'server error' });
     }
   }
 };
